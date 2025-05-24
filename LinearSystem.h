3250bc19bd280bacce10b3281a0d7462441f3f39
KiwisCodes <phanthanhhung.subscription@gmail.com>
2025-05-24 20:12:37 +0700

    added copy constructor to A1, added unary operator overloadings

--- LinearSystem.h
+++ LinearSystem.h
@@ -20,6 +20,35 @@ class Vector:public LinearSystem {
         Vector operator=(const Vector& otherVector){
             return Vector(otherVector.mSize, otherVector.mData);
         }
+
+        Vector(const Vector& otherVector){
+            if(otherVector.mSize == 0 || otherVector.mData == 0) {
+                this->mSize = 0;
+                this->mData = NULL;
+            }
+            else{
+                this->mSize = otherVector.mSize;
+                this->mData = new int[otherVector.mSize+1];
+                for(int i=1;i<otherVector.mSize+1;i++){
+                    this->mData[i] = otherVector.mData[i];
+                }
+            }
+        }
+
+        //unary operators
+        Vector operator+(){
+            return *this;
+        }
+
+        Vector operator-(){
+            if(this->mData == NULL || this->mSize == 0) return NULL;
+            for(int i=1;i<this->mSize+1;i++){
+                this->mData[i] *= -1;
+            }
+            return *this;
+        }
+
+        //binary operators
         Vector operator+(const Vector& otherVector){
             if(this->mSize != otherVector.mSize) {
                 return NULL;
